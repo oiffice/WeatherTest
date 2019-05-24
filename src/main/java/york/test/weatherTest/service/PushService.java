daf35c8167698091f67f765c6001ea97a3ff0c6c
York <york.tsai@fundroots.com>
2019-05-24 15:29:19 +0800

    Change City

--- src/main/java/york/test/weatherTest/service/PushService.java
+++ src/main/java/york/test/weatherTest/service/PushService.java
@@ -4,8 +4,6 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import javax.inject.Inject;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 @Component
 public class PushService {
@@ -13,10 +11,8 @@ public class PushService {
     @Inject
     private WeatherService weatherService;
 
-    @Scheduled(cron = "0 * 8 * * *")
+    @Scheduled(cron = "0 * * * * *")
     public void pushWeatherMessageMorning() throws Exception  {
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-mm-dd HH:mm:ss");
-        System.out.println(simpleDateFormat.format(new Date(System.currentTimeMillis())));
-        weatherService.fetchWeatherData("臺北市", "中山區", ParseInfoService.WEATHER_DESCRIPTION);
+        weatherService.fetchWeatherData("新北市", "汐止區", ParseInfoService.WEATHER_DESCRIPTION);
     }
 }
